c109f1ca929884edf9cdf4e6f2cf38e7f490a613
Eda-prg <edagultopaloglu@gmail.com>
2026-06-04 16:22:51 +0300

    Models ve EncryptionHelper eklendi

---
+++ HMYS.Business/Models/EmailSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMYS.Business.Models
+{
+    public class EmailSettings
+    {
+        public string SenderEmail { get; set; }
+        public string SenderPassword { get; set; }
+        public string SenderName { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
